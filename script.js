07ce8d15a9f96af7409523f03022212e5c40f0a5
bluefish1678 <105519606+bluefish1678@users.noreply.github.com>
2022-07-01 13:16:37 -0400

    uh

---
+++ script.js
@@ -0,0 +1,8 @@
+
+if ((navigator.userAgent.match(/iPhone/i)) || (navigator.userAgent.match(/iPod/i))) {
+window.alert('SITE ISNT MOBILE FREINDLY YET!')
+window.location.replace('https://google.com')
+}
+if (screen.width <= 699) {
+    window.alert('SITE ISNT MOBILE FREINDLY YET!')
+    window.location.replace('https://google.com')    }
